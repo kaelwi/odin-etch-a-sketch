bc3999130771e5b806fa0a72fa8f1ff0add4aca7
Karoline Wild <karoline.elisabeth.wild@seznam.cz>
2022-07-06 10:36:39 +0200

    Implement Rainbow mode and RGB<->HEX conversion

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function createGrid(size) {
     while (grid.firstChild) {
         grid.removeChild(grid.lastChild);
     }
-    
+
     document.documentElement.style.setProperty("--columns-row", size);
 
     for (let i = 0; i < size*size; i++) {
@@ -33,12 +33,23 @@ grid.addEventListener('mousedown', function(e) {
 
 grid.addEventListener('mousemove', function(e) {
     if (isDrawing) {
+        if (mode === 'rainbow') {
+            setRainbowColor();
+        }
         e.target.style.background = bgr;
     }
 })
 
 grid.addEventListener('mouseup', function(e) {
     isDrawing = false;
+})
+
+normal.addEventListener('click', () => {
+    color.value = '#000000';
+    bgr = color.value;
+    document.getElementById(mode).classList.toggle('selected');
+    normal.classList.toggle('selected');
+    mode = 'normal'
 })
 
 erase.addEventListener('click', () => {
@@ -62,6 +73,44 @@ clear.addEventListener('click', () => {
 
 size.addEventListener('change', () => {
     createGrid(size.value);
+})
+
+function setRainbowColor() {
+    console.log(color.value);
+    let r = color.value.slice(1, 3);
+    let g = color.value.slice(3, 5);
+    let b = color.value.slice(5, 7);
+    let colors = [r, g, b];
+
+    
+    colors.forEach( (color, index) => {
+        let random = Math.round(Math.random() * (20 - (-20)) + (-20));
+
+        if ((parseInt(color, 16) + random > 255) || (parseInt(color, 16) + random < 0)) {
+            colors[index] = parseInt(color, 16) - random;
+        } else {
+            colors[index] = parseInt(color, 16) + random;
+        }
+    })
+
+    let hexColor = rgbToHex(colors[0], colors[1], colors[2]);
+    color.value = hexColor;
+    bgr = hexColor;
+}
+
+function componentToHex(c) {
+    var hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+}
+  
+function rgbToHex(r, g, b) {
+    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+
+rainbow.addEventListener('click', () => {
+    document.getElementById(mode).classList.toggle('selected');
+    rainbow.classList.toggle('selected');
+    mode = 'rainbow';
 })
 
 
